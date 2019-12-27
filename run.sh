800ee1809897ab1552f60b3cc0938a1504e5e449
Abhishek Nan <abhisheknan@retinai.cs.ualberta.ca>
2019-12-26 17:12:46 -0700

    setup with bash

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+curl "https://pjreddie.com/media/files/yolov3.weights" --output flask_yolo/yolo_v3/yolov3.weights
+
+sudo yum update -y
+sudo yum install -y docker
+sudo yum install -y git
+
+
+sudo docker build -t custom_yolo:latest .
+
+sudo docker run -d -p 80:80 custom_yolo:latest
